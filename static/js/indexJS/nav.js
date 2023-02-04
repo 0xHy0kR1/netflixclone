9c59495023cc14ac4082e0522dc7ddc3cbc9a990
notFound <helloweenhell666@gmail.com>
2023-02-04 23:18:33 +0530

    nav custom select done now

--- static/js/indexJS/nav.js
+++ static/js/indexJS/nav.js
@@ -1,14 +1,15 @@
-var langSelect = document.getElementById("lang-select");
+var langSelect = document.getElementById("language-select");
 var currentLang = document.getElementById("current-lang");
 var options = document.getElementsByClassName("options");
 var langList = document.getElementById("lang-list");
+
+langSelect.onclick = function(){
+    console.log("me")
+    langList.classList.toggle("hide-lang-list");
+}
 
 for (const option of options) {
     option.onclick = function(){
         currentLang.innerHTML = this.textContent;
     }
 }
-
-langSelect.addEventListener('click', function(e){
-    langList.classList.toggle("hide-lang-list");
-});
