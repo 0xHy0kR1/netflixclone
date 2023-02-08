7b601d7d221200669d3e2a77cdeb95f8bff65e93
notFound <helloweenhell666@gmail.com>
2023-02-08 23:24:16 +0530

    navbar new design formed now with new arrow and background and globe

--- static/js/indexJS/nav.js
+++ static/js/indexJS/nav.js
@@ -1,13 +1,17 @@
-var langSelect = document.getElementById("language-select");
-var currentLang = document.getElementById("current-lang");
-var options = document.getElementsByClassName("options");
+// The below JS code is only for custom select language
+
+
+var langSelect = document.getElementById("language-select");// The displayed language box id 
+var currentLang = document.getElementById("current-lang");// As the name depicts
+var options = document.getElementsByClassName("options");// same as previous
 var langList = document.getElementById("lang-list");
 
+
 langSelect.onclick = function(){
-    console.log("me")
-    langList.classList.toggle("hide-lang-list");
+    langList.classList.toggle("hide-lang-list");// It will help us when we click on same language select box then it will hide and also show the hindi and english langu.
 }
 
+// from the below code we select the language between english and hindi
 for (const option of options) {
     option.onclick = function(){
         currentLang.innerHTML = this.textContent;
