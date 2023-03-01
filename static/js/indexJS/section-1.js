04f6f98067caefb0ab35089c72f0c9b42fe3f2a6
notFound <helloweenhell666@gmail.com>
2023-03-01 23:22:50 +0530

    email box css finally done but need to write redirect page js

--- static/js/indexJS/section-1.js
+++ static/js/indexJS/section-1.js
@@ -8,3 +8,36 @@ inputGreyBorder.addEventListener('focus', () =>{
 inputGreyBorder.addEventListener('blur', () =>{
     divWhiteBorder.style.border = "none";
 })
+
+// Writing js for label when user clicked on email box
+const emailInput = document.getElementById("email-of-user");
+const emailLabel = document.querySelector(".label-email-name");
+
+emailInput.addEventListener('focus', () => {
+    emailLabel.classList.add('label-move');
+});
+
+emailInput.addEventListener('blur', () => {
+    if(emailInput.value == ''){
+        emailLabel.classList.remove('label-move');
+    }
+});
+// Writing js for validating email and if email is correct then redirecting to another page
+const form = document.querySelector('#index-page-email');
+
+form.addEventListener('submit', () =>{
+    e.preventDefault();
+
+    const emailInput = document.querySelector('#email-of-user');
+    const email = emailInput.value.trim();
+
+    // Validate email address
+    const emailRegex = `/^[^\s@]+@[^\s@]+\.[^\s@]+$/`;
+    if (emailRegex.test(email)) {
+        // Redirect to second page
+        window.location.href = 'signup-step-1.html';
+    }
+    else{
+        // Display the error message
+    }
+})
