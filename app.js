145423d6239e695f2b41a4fd3a395102a5751506
notFound <helloweenhell666@gmail.com>
2023-03-28 23:22:58 +0530

    finally connected to mongodb database and able to show the signup page

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+// Require the necessary packages in your Node.js app.
 const express = require("express"); // Importing the express module
 const path = require("path"); // We import this module to give path to the view directory
+const bodyParser = require('body-parser'); // We import this to parse the form data because node.js doesn't include request body data that's why we use middleware
+const mongoose = require('mongoose'); // By importing mongoose in our Node.js application, we can use it to create models for our data, connect to a MongoDB database, and perform CRUD (Create, Read, Update, and Delete) operations on our data.
+
+// Set up the Express app and middleware stuffs
 const app = express(); // making the express app
+
+app.use(bodyParser.urlencoded({extended: true})); //  we import the body-parser middleware to extract the data submitted in a POST request from the request body, and we use it in our Express app by calling app.use().
+app.use(bodyParser.json()); // it tells our Express app to use the body-parser middleware to parse JSON data in the request body of incoming HTTP requests. 
+
 const port = 80;
 
 // For Serving static files
@@ -17,6 +26,51 @@ app.set('views', path.join(__dirname, 'views'))
 //     res.status(200).render('index', {title: 'netflix', message: 'Hello there!'})
 // });
 
+// Connecting to the MongoDB database using Mongoose
+// The below help us to estaiblish connection to the "netflix-index-email" database on the MongoDB server.
+// Note - the `useNewUrlParser` and `useUnifiedTopology` options to avoid deprecation warnings
+mongoose.connect('mongodb://localhost:27017/netflix',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection; // This means that `db` is now a shorter way to refer to the connection object which is created by Mongoose
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to database');
+});
+
+// Defining a schema for our email collection using Mongoose
+const emailSchema = new mongoose.Schema({
+    email: {
+        type: String, 
+        required: true,
+    },
+});
+// Creating a table from the above schema
+const Email = mongoose.model('Email', emailSchema);
+
+app.post('/', (req, res) => {
+    const email = new Email({
+        email: req.body.email,
+    });
+    console.log(email.email);
+    email.save()
+        .then(() => {
+            res.status(200).render('signup.pug');
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        // if(err){
+        //     console.error(err);
+        //     res.sendStatus(500);
+        // }
+        // else{
+        //     res.status(200).render('signup.pug ', {title: 'Success' });
+        // }
+    });
 app.get('/', (req, res) =>{
     const con = "This is a content"
     const params = {'title': 'Netflix is a title', "content": con}
